0d1a54fc94881bec6d644757a2cf6e1a18552e1f
Prajwal_9942 <patilprajwal9942@gmail.com>
2026-06-10 18:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-const codes = document.querySelectorAll('.code');
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
 
 codes.forEach((code, idx) => {
-    code.addEventListener('input', () => {
+
+    code.addEventListener("input", () => {
         if (code.value.length === 1 && idx < codes.length - 1) {
             codes[idx + 1].focus();
         }
     });
+
+    code.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace" && code.value === "" && idx > 0) {
+            codes[idx - 1].value = "";
+            codes[idx - 1].focus();
+        }
+    });
+
 });
