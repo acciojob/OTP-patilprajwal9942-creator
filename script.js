76fe1a88b79c400aa0dbd3967b8f8ff1d3158cff
Prajwal_9942 <patilprajwal9942@gmail.com>
2026-06-10 17:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+const code = document.queryselectorAll('.code');
+
+code.foreach((code, idx)=>{
+	code.addEventListener("input", () => { 
+    if (code.value.length === 1 && idx < codes.length - 1) {
+      codes[idx + 1].focus(); 
+    }
+})
